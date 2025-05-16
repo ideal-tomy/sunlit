52bbc249be65d4d6059cd2866c797409d620219b
ideal-tomy <ryojitomii@gmail.com>
2025-05-16 23:40:23 +0900

    修正11

--- api/dify-chat.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  console.log("!!! api/dify-chat.js (pure JS) handler was called !!!");
-  res.status(200).json({ message: "Hello from pure ESM JS on Vercel!" });
-}
